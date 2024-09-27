0c763e40a72939ce5c755271b713121335b8a7ae
namycodes <namy.hacker0@gmail.com>
2024-09-28 00:33:11 +0200

    Added Laoding Indicator on the login button

--- app/page.js
+++ app/page.js
@@ -207,11 +207,11 @@ export default function Home() {
 						</div>
 					</div>
 					<h1>Today{"'"}s Highlights</h1>
-					<div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+					<div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
 						{isFetching || isRefetching ? (
 							<>
 								{Array.from({ length: 6 }).map((index) => (
-									<Skeleton key={index} className="bg-white/20 h-[100px] w-[180px] rounded-lg" />
+									<Skeleton key={index} className="bg-white/20 h-[100px] w-full lg:w-[180px] rounded-lg" />
 								))}
 							</>
 						) : (
